3f1ab6300852944448008c11833a32bf59af55e0
Dave Longley <dlongley@digitalbazaar.com>
2019-02-06 19:43:13 -0500

    Add core files.

---
+++ test/components/BrRoot.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout>
+
+    <q-layout-header>
+      <q-toolbar>
+        <q-toolbar-title>
+          Bedrock Create Organization Wizard Test
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+
+    <q-layout-footer>
+    </q-layout-footer>
+
+  </q-layout>
+</template>
+<script>
+export default {
+  name: 'BrRoot'
+};
+</script>
+<style>
+</style>
