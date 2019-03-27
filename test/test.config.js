2ad6a32fb633e31516f8562a7494d680833ad1e7
andrewjones <ajones@digitalbazaar.com>
2019-03-27 09:20:18 -0600

    Shorten regex for quasar to just quasar$.

--- test/test.config.js
+++ test/test.config.js
@@ -12,7 +12,7 @@ config.karma.suites['bedrock-vue-create-organization-wizard'] =
 // TODO: consider moving to `bedrock-karma`?
 config.karma.config.webpack.resolve = {
   alias: {
-    'quasar-$': path.resolve(
+    quasar$: path.resolve(
       __dirname, 'node_modules/quasar/dist/umd/quasar.mat.umd.js')
   }
 };
